51ab96a32f323f155a2000efaadbed9855c61291
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-07-07 09:25:28 +0300

    :star: (feat: Hero): animation for text and actions

--- src/scenes/Hero/HeroActions.tsx
+++ src/scenes/Hero/HeroActions.tsx
@@ -1,6 +1,7 @@
 import ActionButton from "@/shared/ActionButton";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -9,7 +10,17 @@ type Props = {
 const HeroActions: React.FC<Props> = ({ setSelectedPage }) => {
   return (
     <>
-      <div className="mt-8 flex items-center gap-8">
+      <motion.div
+        className="mt-8 flex items-center gap-8"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ delay: 0.2, duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
         <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
 
         <AnchorLink
@@ -19,7 +30,7 @@ const HeroActions: React.FC<Props> = ({ setSelectedPage }) => {
         >
           <p>Learn More</p>
         </AnchorLink>
-      </div>
+      </motion.div>
     </>
   );
 };
--- src/scenes/Hero/HeroTextBlock.tsx
+++ src/scenes/Hero/HeroTextBlock.tsx
@@ -1,8 +1,19 @@
 import HomePageText from "@/assets/HomePageText.png";
+import { motion } from "framer-motion";
 
 const HeroTextBlock = () => {
   return (
-    <div className="md:-mt-20">
+    <motion.div
+      className="md:-mt-20"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.5 }}
+      transition={{ duration: 0.5 }}
+      variants={{
+        hidden: { opacity: 0, x: -50 },
+        visible: { opacity: 1, x: 0 },
+      }}
+    >
       <div className="relative">
         <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] md:before:content-evolvetext">
           <img src={HomePageText} alt="home-page-text" />
@@ -14,7 +25,7 @@ const HeroTextBlock = () => {
         Studios to get the Body Shapes That you Dream of.. Get Your Dream Body
         Now.
       </p>
-    </div>
+    </motion.div>
   );
 };
 
