5007fd05ef9703a6c26a01f2ef410e604bc6bd90
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-07-07 12:56:46 +0300

    :hammer: (ref: Hero): add shared LearnMoreLink

--- src/scenes/Hero/HeroActions.tsx
+++ src/scenes/Hero/HeroActions.tsx
@@ -2,6 +2,7 @@ import ActionButton from "@/shared/ActionButton";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
+import LearnMoreLink from "@/shared/LearnMoreLink";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -23,13 +24,7 @@ const HeroActions: React.FC<Props> = ({ setSelectedPage }) => {
       >
         <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
 
-        <AnchorLink
-          className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
-          onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-          href={`#${SelectedPage.ContactUs}`}
-        >
-          <p>Learn More</p>
-        </AnchorLink>
+        <LearnMoreLink setSelectedPage={setSelectedPage} />
       </motion.div>
     </>
   );
